c767f1b66871211b578cad2f7cf93bcee26039c1
Jack <jchen.smr24sl@gmail.com>
2026-03-04 17:02:41 +0100

    subida final

---
+++ README.txt
@@ -0,0 +1,19 @@
+En el http://localhost/phpmyadmin hay que crear varios bases de datos:
+
+CREATE DATABASE IF NOT EXISTS `ToDoList`;
+USE `ToDoList`;
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `todo` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(255) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- perfil.php
+++ perfil.php
@@ -1,12 +1,47 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.html");
+    exit();
+}
+
+$host = 'localhost';
+$db   = 'sistema_login';
+$user = 'root';
+$pass = '';
+$conn = new mysqli($host, $user, $pass, $db);
+
+$usuario_id = $_SESSION['user_id'];
+$username = $_SESSION['username'];
+
+$sql = "SELECT created_at FROM usuarios WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $usuario_id);
+$stmt->execute();
+
+$resultado = $stmt->get_result();
+$datos_usuario = $resultado->fetch_assoc();
+$fecha_registro = $datos_usuario['created_at'] ?? 'No disponible';
+$fecha_formateada = date("d/m/Y", strtotime($fecha_registro));
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mi Perfil - To Do List</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <div class="todo">
+        <div class="avatar">
+        </div>
+        <br><br><br>
+        <h2>User Profile</h2>
+        <p><strong>Username</strong>: <?php echo htmlspecialchars($username); ?></p>
+        <p><strong>Create Date</strong>: <?php echo htmlspecialchars($fecha_formateada);?></p> <br>
+        <a href="Todo.php" class="boton">Regresar</a>
+    </div>
 </body>
 </html>
